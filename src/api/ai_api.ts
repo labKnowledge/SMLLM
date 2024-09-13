af0a1871044d3c5633c701780da6164b4a0b8c7e
Remy Gakwaya <32406787+labKnowledge@users.noreply.github.com>
2024-09-12 22:07:38 -0600

    openai initialization error management

--- src/api/ai_api.ts
+++ src/api/ai_api.ts
@@ -1,12 +1,18 @@
 import OpenAI from 'openai';
 
-const openai = new OpenAI({
-  apiKey: import.meta.env.VITE_APIKEY, 
-  baseURL: import.meta.env.VITE_BASE_URL
-});
+let openai = null
 
+  try{
+    openai = new OpenAI({
+      apiKey: import.meta.env.VITE_APIKEY, 
+      baseURL: import.meta.env.VITE_BASE_URL
+    });
+  }catch(error){
+    console.log("openAI initialization failed", error)
+  }
+  
 export const completeCall = async (txt: string) => {
-  const response = await openai.chat.completions.create({
+  const response = await openai?.chat.completions.create({
     messages: [
       { role: "system", content: import.meta.env.VITE_SYSTEM_MESSAGE},
       { role: "user", content: txt }
@@ -18,7 +24,7 @@ export const completeCall = async (txt: string) => {
 };
 
 export const streamChat = async (messages: OpenAI.Chat.ChatCompletionMessageParam[], onChunk: (chunk: string) => void) => {
-  const stream = await openai.chat.completions.create({
+  const stream = await openai?.chat.completions.create({
     messages,
     model: import.meta.env.VITE_AIMODEL,
     temperature: 0.7,
